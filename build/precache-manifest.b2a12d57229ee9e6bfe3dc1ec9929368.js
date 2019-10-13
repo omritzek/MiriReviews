bf25c3e2013f16f209d73b08e13f2e38f000d719
omritzek <omritzek@gmail.com>
2019-10-13 16:42:28 +0300

    build

---
+++ build/precache-manifest.b2a12d57229ee9e6bfe3dc1ec9929368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d4fb73c95e2a1c8a39986c138cdd34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f5943280d9625cf8c60",
+    "url": "/static/css/main.eb52bfb5.chunk.css"
+  },
+  {
+    "revision": "aebbe1944b204cb2c463",
+    "url": "/static/js/2.ae2f5cb5.chunk.js"
+  },
+  {
+    "revision": "1f5943280d9625cf8c60",
+    "url": "/static/js/main.be09fa0f.chunk.js"
+  },
+  {
+    "revision": "71896b1aafa26024bf96",
+    "url": "/static/js/runtime-main.710a885c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
